416301b53198d453d17d883c693636bfd1ec40d0
Leekb0804 <lkb8713@gmail.com>
2024-04-12 20:16:31 +0900

    add gradeQ2 ~ Q5

--- src/controllers/S1003Controller.js
+++ src/controllers/S1003Controller.js
@@ -18,11 +18,11 @@ exports.grade = async (req, res) => {
     res.json({ success: false });
 };
 
-// 1번 문항에 대한 정답 판별 로직
-async function gradeQ1() {
+// 2번 문항에 대한 정답 판별 로직
+async function gradeQ2() {
     try {
         const { stdout, stderr } = await execAsync(
-            "diff -bwu ./src/public/A ./src/public/B"
+            "diff /usr/stage_file/Q2/Hello_answer.txt /home/$stage/test/Hello.txt"
         );
         console.log(stdout);
 
@@ -35,6 +35,56 @@ async function gradeQ1() {
     }
 }
 
+// 3번 문항에 대한 정답 판별 로직
+async function gradeQ3() {
+    try {
+        const { stdout, stderr } = await execAsync(
+            "diff /usr/stage_file/Q3/Hello_answer.txt /home/$stage/test/Hello.txt"
+        );
+        console.log(stdout);
+
+        const result = !stdout; // stdout 존재유무로 diff 판단
+        console.log(`[grade] result: ${result}`);
+        return result;
+    } catch (error) {
+        console.error(`[grade] error: ${error}`);
+        return false;
+    }
+}
+
+// 4번 문항에 대한 정답 판별 로직
+async function gradeQ3() {
+    try {
+        const { stdout, stderr } = await execAsync(
+            "diff /usr/stage_file/Q4/original.txt /home/$stage/test/copy.txt"
+        );
+        console.log(stdout);
+
+        const result = !stdout; // stdout 존재유무로 diff 판단
+        console.log(`[grade] result: ${result}`);
+        return result;
+    } catch (error) {
+        console.error(`[grade] error: ${error}`);
+        return false;
+    }
+}
+
+// 5번 문항에 대한 정답 판별 로직
+async function gradeQ3() {
+    try {
+        const { stdout, stderr } = await execAsync(
+            "diff /usr/stage_file/Q5/answer.txt /home/$stage/test/sample2.txt"
+        );
+        console.log(stdout);
+
+        const result = !stdout; // stdout 존재유무로 diff 판단
+        console.log(`[grade] result: ${result}`);
+        return result;
+    } catch (error) {
+        console.error(`[grade] error: ${error}`);
+        return false;
+    }
+}
 
 // 환경구성 받는 컨트롤 메서드
 exports.compose = async (req, res) => {
