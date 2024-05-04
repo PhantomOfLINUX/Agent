0b2dafbd6dffe12b7e4d5c707579ef26a72b77ba
Jeong-Rae <kkwjdfo@gmail.com>
2024-05-05 05:14:19 +0900

    [Feat] count 라우터 수정

--- src/routes/userCounter.js
+++ src/routes/userCounter.js
@@ -5,7 +5,7 @@ const counter = require("../models/clientCounter");
 
 clientCountRouter.get("/client", async (req, res) => {
     try {
-        res.status(200).json({ usersCount: counter.getCount });
+        res.status(200).json({ usersCount: counter.getCount() });
     } catch (error) {
         console.error(`[clientCountRouter] error: ${error}`);
         res.status(500).json({ error: "Internal Server Error" });
